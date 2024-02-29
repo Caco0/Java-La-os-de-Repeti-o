cfc0b8a8b3257e66183ec3214974e4f422257109
Caco0 <cacotpds@gmail.com>
2024-02-29 14:35:16 -0300

    break continue

---
+++ For/src/BreakContinue.java
@@ -0,0 +1,14 @@
+public class BreakContinue {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            if (i == 5) {
+                // break;
+                continue;
+
+            }
+
+            System.out.println(i);
+
+        }
+    }
+}
