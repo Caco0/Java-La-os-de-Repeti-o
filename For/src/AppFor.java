fdb9aa61554fce53abb10e383d6febd8fd6dc5c4
Caco0 <cacotpds@gmail.com>
2024-02-29 11:11:46 -0300

    structure for arrays

---
+++ For/src/AppFor.java
@@ -0,0 +1,8 @@
+public class AppFor {
+    public static void main(String[] args) throws Exception {
+        for ( int carneiros = 1; carneiros<=20; carneiros++) {
+            System.out.println("Contando carneirinhos " + carneiros);
+        }
+        System.out.println("Joãozinho Dormiu!");
+    }
+}
