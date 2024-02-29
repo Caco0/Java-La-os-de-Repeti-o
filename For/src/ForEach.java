2d5fdd6561447095a5a268961a815357f05c4f54
Caco0 <cacotpds@gmail.com>
2024-02-29 11:28:25 -0300

    For Each Repetition Structure

---
+++ For/src/ForEach.java
@@ -0,0 +1,9 @@
+public class ForEach {
+    public static void main(String[] args) {
+        String alunos[] = {"Aline", "Felipe", "Rafael", "Soraya"};
+
+        for (String aluno : alunos) { 
+            System.out.println("O aluno do indice i é: " + aluno); 
+        }
+    }
+}
--- For/src/ForEmArrays.java
+++ For/src/ForEmArrays.java
@@ -1,6 +1,6 @@
 public class ForEmArrays {
     public static void main(String[] args) {
-        String alunos[] = {"Aline", "Felipe","Rafael", "Soraya"};
+        String alunos[] = {"Aline", "Felipe", "Rafael", "Soraya"};
 
         for (int i = 0; i < alunos.length; i++) { 
             System.out.println("O aluno do indice i é: " + i + " " + alunos[i]); //imprime o indíce e o nome do aluno 
